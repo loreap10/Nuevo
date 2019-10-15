5c1e6b69b1e264dfa850b2bd9b761286797e7e63
loreap10 <lorealiendre@gmail.com>
2019-10-15 14:26:50 -0400

    Segunda

--- src/model/Sonido.java
+++ src/model/Sonido.java
@@ -1,5 +1,18 @@
 package model;
 
-public class Sonido {
+import java.io.IOException;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Sonido 
+{
+	public void sonido() throws LineUnavailableException, IOException, UnsupportedAudioFileException
+	{
+		Clip sonido=AudioSystem.getClip();
+		sonido.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream("sonido/africandrums.wav")));
+		sonido.start();
+	}
 }
